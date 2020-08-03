99e72d29d9a31738bffc3b885f3b1e6515a06487
Nazareno Ciancaglini <nazareno.ciancaglini@gmail.com>
2020-08-03 19:13:06 +0200

    orderBook update: currentMarketBidPrecision

--- src/components/OrderForm/index.tsx
+++ src/components/OrderForm/index.tsx
@@ -255,11 +255,11 @@ export class OrderForm extends React.PureComponent<OrderFormProps, OrderFormStat
                         <div className="cr-order-item__total__content">
                             {orderType === 'Limit' ? (
                                 <span className="cr-order-item__total__content__amount">
-                                    {total.toFixed(type === 'buy' ? currentMarketAskPrecision : currentMarketBidPrecision)}
+                                    {total.toFixed(currentMarketBidPrecision)}
                                 </span>
                             ) : (
                                 <span className="cr-order-item__total__content__amount">
-                                    &asymp;{total.toFixed(type === 'buy' ? currentMarketAskPrecision : currentMarketBidPrecision)}
+                                    &asymp;{total.toFixed(currentMarketBidPrecision)}
                                 </span>
                             )}
                             <span className="cr-order-item__total__content__currency">
